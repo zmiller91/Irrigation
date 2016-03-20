4163263902fbdf3f4be58a2d660bd17f94eacdf8
= <=>
2016-03-19 19:20:14 -0600

    push from the raspberry pi, made a bunch of changes (as in, entirely redid) main.py

--- main.py
+++ main.py
@@ -1,7 +1,29 @@
 __author__ = 'zmiller'
 
 import serial
+import requests
+import json
+from time import sleep
+from datetime import datetime
 
-ser = serial.Serial('COM3', 9600)
+ser = serial.Serial('/dev/ttyACM0', 9600)
 while True:
-    print ser.readline()
+
+    try:
+        dt = str(datetime.now())
+
+        out = {
+            "date": str(datetime.now()), 
+            "method": "arduino_post", 
+            "values": ser.readline().rstrip("\r\n")
+        }
+
+        out = json.dumps(out)
+        requests.post("http://45.79.167.160", data=out)
+        print out
+        sleep(0.1)
+    except Exception as e:
+        with open("err.txt", "a") as file:
+            file.write(e);
+            file.write("\n");
+            file.close();
